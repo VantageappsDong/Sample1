09f4094cd70168e1ce84837766a367cfc415da79
VantageappsDong <dongho.kim@vantageapps.co.jp>
2018-03-19 21:07:08 +0900

    Create

---
+++ Sample1/Sample1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample1
+//
+//  Created by MP0004 on 2018/03/19.
+//  Copyright © 2018 vr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
